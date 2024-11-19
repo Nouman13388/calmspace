9196b0ed930196ec8b3027932d8862d8ac89bbb8
Nouman13388 <nouman.nomi1338@gmail.com>
2024-11-19 23:51:15 +0500

    therapist profile fixed as well

--- lib/controllers/therapist_profile_controller.dart
+++ lib/controllers/therapist_profile_controller.dart
@@ -1,10 +1,223 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io'; // For handling File
 
-class TherapistProfileController extends StatelessWidget {
-  const TherapistProfileController({super.key});
+import 'package:calmspace/controllers/therapist_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart'; // For picking images
+import 'package:intl/intl.dart'; // For date formatting (optional)
+
+import '../constants/app_constants.dart'; // Add the correct path for your constants
+import 'user_controller.dart'; // Import the UserController to get logged-in user's email
+
+class TherapistProfileController extends GetxController {
+  var isLoading = true.obs; // Loading state for UI
+  var isEditMode = false.obs; // Edit mode flag
+  var profile = Rxn<Therapist>(); // To hold the profile data
+  var profilePicture = ''.obs; // To hold the image path
+  var username = ''.obs;
+  var location = ''.obs; // To hold location (latitude, longitude)
+  var bio = ''.obs;
+  var specialization = ''.obs; // New specialization field
+  var formKey = GlobalKey<FormState>(); // Form validation key
+  var pickedImage = Rxn<File>(); // For storing the picked image
+
+  final picker = ImagePicker(); // Image picker instance
+
+  final UserController userController =
+      Get.find<UserController>(); // Get UserController instance
+  final TherapistController therapistController =
+      Get.find<TherapistController>();
+
+  // Fetch Therapist profile details
+  Future<void> fetchTherapistProfile() async {
+    final userId = await userController.getLoggedInUserId();
+    final therapistId = await therapistController.getLoggedInTherapistId();
+
+    if (userId == null) {
+      print('No logged-in user found!');
+      return;
+    }
+
+    print("Fetching therapist profile for userId: $userId");
+
+    isLoading.value = true;
+
+    try {
+      final response = await Future.wait([
+        http.get(Uri.parse('${AppConstants.profilesUrl}?user=$userId')),
+        http.get(Uri.parse('${AppConstants.usersUrl}?id=$userId')),
+        http.get(Uri.parse('${AppConstants.professionalsUrl}?id=$therapistId')),
+      ]);
+
+      if (response.every((r) => r.statusCode == 200)) {
+        final profileData = json.decode(response[0].body);
+        final userData = json.decode(response[1].body);
+        final professionalData = json.decode(response[2].body);
+
+        print("Therapist profile data fetched: $profileData");
+        print("User data fetched: $userData");
+        print("Professional data fetched: $professionalData");
+
+        if (profileData.isNotEmpty &&
+            userData.isNotEmpty &&
+            professionalData.isNotEmpty) {
+          final userName = userData[0]['name'] as String;
+          profile.value = Therapist.fromJson(profileData[0], name: userName);
+
+          // Set initial values for form fields
+          username.value = profile.value?.name ?? '';
+          location.value = profile.value?.location ?? '';
+          bio.value = professionalData[0]['bio'] ??
+              ''; // Using bio from professionalsUrl
+          specialization.value = professionalData[0]['specialization'] ??
+              ''; // New specialization field
+          profilePicture.value = profile.value?.profilePicture ?? '';
+
+          print("Therapist profile loaded successfully: ${profile.value}");
+        } else {
+          print("No therapist profiles or user data found.");
+          resetProfileFields();
+        }
+      } else {
+        print(
+            "Failed to load profile data. Status code: ${response[0].statusCode}");
+        throw Exception('Failed to load therapist profile');
+      }
+    } catch (e) {
+      print("Error fetching therapist profile: $e");
+    } finally {
+      isLoading.value = false;
+      print("Finished loading therapist profile");
+    }
+  }
+
+  void resetProfileFields() {
+    username.value = '';
+    location.value = '';
+    bio.value = '';
+    specialization.value = '';
+    profilePicture.value = '';
+  }
+
+  // Save the updated therapist profile
+  Future<void> saveProfile() async {
+    print("Saving therapist profile...");
+
+    final loggedInEmail =
+        (await userController.getLoggedInUserId()) ?? 'therapist@example.com';
+
+    final updatedProfile = {
+      'user': loggedInEmail,
+      'bio': bio.value,
+      'specialization': specialization.value,
+      'location': location.value,
+      'profile_picture': pickedImage.value?.path ?? profilePicture.value,
+      'privacy_settings': 'Public',
+      'created_at': DateFormat('yyyy-MM-ddTHH:mm:ssZ').format(DateTime.now()),
+      'updated_at': DateFormat('yyyy-MM-ddTHH:mm:ssZ').format(DateTime.now()),
+    };
+
+    try {
+      final isProfileNew = profile.value == null;
+      final url = isProfileNew
+          ? AppConstants.profilesUrl
+          : '${AppConstants.profilesUrl}${profile.value?.id}/';
+      final requestMethod = isProfileNew ? 'POST' : 'PUT';
+
+      var request = http.MultipartRequest(requestMethod, Uri.parse(url));
+
+      // Add fields to the request
+      updatedProfile.forEach((key, value) {
+        if (value != null) request.fields[key] = value.toString();
+      });
+
+      if (pickedImage.value != null) {
+        var pic = await http.MultipartFile.fromPath(
+            'profile_picture', pickedImage.value!.path);
+        request.files.add(pic);
+      }
+
+      print("Sending request to: ${request.url}");
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        print("Therapist profile saved successfully!");
+        Get.snackbar("Success", "Profile saved successfully!",
+            backgroundColor: Colors.green, colorText: Colors.white);
+      } else {
+        print("Failed to save profile. Status code: ${response.statusCode}");
+        final responseBody = await response.stream.bytesToString();
+        print("Response body: $responseBody");
+        throw Exception("Failed to save therapist profile");
+      }
+    } catch (e) {
+      print("Error saving profile: $e");
+    }
+  }
+
+  // Toggle between edit and view mode
+  void toggleEditMode() {
+    isEditMode.value = !isEditMode.value;
+    print("Edit mode toggled: ${isEditMode.value}");
+  }
+
+  // Pick an image (image picker logic)
+  Future<void> pickImage() async {
+    print("Picking image...");
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      final file = File(pickedFile.path);
+      if (file.lengthSync() > 1024 * 1024 * 5) {
+        print("Image file is too large!");
+        return; // Reject if the image is too large (5MB here)
+      }
+
+      pickedImage.value = file;
+      print("Picked image: ${pickedImage.value?.path}");
+    } else {
+      print("No image selected.");
+    }
+  }
+}
+
+class Therapist {
+  final int id;
+  final String name;
+  final String email;
+  final String bio;
+  final String location;
+  final String profilePicture;
+  final String specialization;
+
+  Therapist({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.bio,
+    required this.location,
+    required this.profilePicture,
+    required this.specialization,
+  });
+
+  // Factory constructor to parse JSON response from API
+  factory Therapist.fromJson(Map<String, dynamic> json, {String? name}) {
+    return Therapist(
+      id: json['id'] as int,
+      name: name ?? 'No Name',
+      email: json['email'] as String? ?? 'No Email',
+      bio: json['bio'] as String? ?? 'No Bio',
+      location: json['location'] as String? ?? 'No Location',
+      profilePicture: json['profile_picture'] as String? ??
+          'https://via.placeholder.com/150',
+      specialization: json['specialization'] as String? ?? 'No Specialization',
+    );
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  String toString() {
+    return 'Therapist(id: $id, name: $name, email: $email, bio: $bio, location: $location, profilePicture: $profilePicture, specialization: $specialization)';
   }
 }
--- lib/views/profile_pages/therapist_profile_page.dart
+++ lib/views/profile_pages/therapist_profile_page.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../controllers/profile_controller.dart';
+import '../../controllers/therapist_profile_controller.dart'; // Ensure correct import path
 
 class TherapistProfilePage extends StatelessWidget {
   final TherapistProfileController controller =
@@ -13,104 +11,261 @@ class TherapistProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    controller.fetchTherapistProfile(); // Fetch profile data
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User Profile'),
-        leading: Obx(() => controller.isEditing.value
-            ? IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: controller.toggleEditing,
-              )
-            : Container()),
+        title: const Text("Therapist Profile"),
         actions: [
-          Obx(() => !controller.isEditing.value
-              ? IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: controller.toggleEditing,
-                )
-              : Container()),
+          // Show the edit button only when not in edit mode
+          Obx(() {
+            return Visibility(
+              visible: !controller.isEditMode.value,
+              child: IconButton(
+                icon: const Icon(Icons.edit, color: Colors.white),
+                onPressed: () {
+                  controller.toggleEditMode(); // Enter edit mode
+                },
+              ),
+            );
+          }),
+          // Show the save button only when in edit mode
+          Obx(() {
+            return Visibility(
+              visible: controller.isEditMode.value,
+              child: IconButton(
+                icon: const Icon(Icons.check, color: Colors.white),
+                onPressed: () {
+                  if (controller.formKey.currentState?.validate() ?? false) {
+                    controller.saveProfile(); // Save the profile
+                    controller.toggleEditMode(); // Exit edit mode after saving
+                  }
+                },
+              ),
+            );
+          }),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Obx(() {
-          return Form(
-            child: ListView(
-              children: [
-                Center(
-                  child: GestureDetector(
-                      onTap: controller.isEditing.value
-                          ? controller.pickImage
-                          : null,
-                      child: CircleAvatar(
-                        radius: 100,
-                        backgroundImage:
-                            controller.photoUrl.value.startsWith('http')
-                                ? NetworkImage(controller.photoUrl.value)
-                                : FileImage(File(controller.photoUrl.value)),
-                        child: controller.photoUrl.value.isEmpty &&
-                                controller.isEditing.value
-                            ? const Icon(Icons.camera_alt, color: Colors.white)
-                            : null,
-                      )),
-                ),
-                const SizedBox(height: 16.0),
-                controller.isEditing.value
-                    ? TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Username',
-                          border: OutlineInputBorder(),
-                        ),
-                        initialValue: controller.username.value,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a username';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          controller.username.value = value;
-                        },
-                      )
-                    : ListTile(
-                        title: const Text('Username'),
-                        subtitle: Text(controller.username.value),
-                      ),
-                const SizedBox(height: 16.0),
-                controller.isEditing.value
-                    ? TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                        ),
-                        initialValue: controller.email.value,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter an email';
-                          }
-                          if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                            return 'Please enter a valid email address';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          controller.email.value = value;
-                        },
-                      )
-                    : ListTile(
-                        title: const Text('Email'),
-                        subtitle: Text(controller.email.value),
-                      ),
-                const SizedBox(height: 16.0),
-                if (controller.isEditing.value)
-                  ElevatedButton(
-                    onPressed: controller.saveProfile,
-                    child: const Text('Save Profile'),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+          child: ListView(
+            children: [
+              Center(
+                child: GestureDetector(
+                  onTap: controller.isEditMode.value
+                      ? controller
+                          .pickImage // Open image picker only if in edit mode
+                      : null,
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: controller.pickedImage.value == null
+                        ? NetworkImage(
+                            controller.profile.value?.profilePicture ??
+                                'https://via.placeholder.com/150')
+                        : FileImage(controller.pickedImage.value!)
+                            as ImageProvider,
+                    child: controller.isEditMode.value &&
+                            controller.pickedImage.value == null
+                        ? const Icon(Icons.camera_alt,
+                            color: Colors.white,
+                            size: 30) // Show camera icon only in edit mode
+                        : null,
                   ),
-              ],
-            ),
-          );
-        }),
+                ),
+              ),
+              const SizedBox(height: 25),
+              controller.profile.value != null
+                  ? (controller.isEditMode.value
+                      ? _buildEditForm()
+                      : _buildProfileDetails())
+                  : _buildNoProfilePrompt(),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  Widget _buildProfileDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildProfileDetailRow(
+            Icons.account_circle, 'Username', controller.username.value),
+        const SizedBox(height: 12),
+        _buildProfileDetailRow(
+            Icons.location_on, 'Location', controller.location.value),
+        const SizedBox(height: 12),
+        _buildProfileDetailRow(
+            Icons.star, 'Specialization', controller.specialization.value),
+        const SizedBox(height: 12),
+        _buildProfileDetailRow(Icons.edit, 'Bio', controller.bio.value),
+      ],
+    );
+  }
+
+  Widget _buildProfileDetailRow(IconData icon, String label, String value) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.grey[700], size: 20),
+        const SizedBox(width: 10),
+        Text(
+          '$label: ',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Colors.black87,
+          ),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: const TextStyle(fontSize: 16, color: Colors.black54),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildEditForm() {
+    return Form(
+      key: controller.formKey,
+      child: Column(
+        children: [
+          _buildTextFormField(
+            'Username',
+            controller.username.value,
+            (value) {
+              controller.username.value = value;
+            },
+            icon: Icons.account_circle,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Location',
+            controller.location.value,
+            (value) {
+              controller.location.value = value;
+            },
+            icon: Icons.location_on,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Specialization',
+            controller.specialization.value,
+            (value) {
+              controller.specialization.value = value;
+            },
+            icon: Icons.star,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Bio',
+            controller.bio.value,
+            (value) {
+              controller.bio.value = value;
+            },
+            icon: Icons.edit,
+            maxLines: 4,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextFormField(
+    String label,
+    String initialValue,
+    Function(String) onChanged, {
+    int maxLines = 1,
+    required IconData icon,
+  }) {
+    return TextFormField(
+      initialValue: initialValue,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(fontWeight: FontWeight.w500),
+        border: const OutlineInputBorder(),
+        prefixIcon: Icon(icon, color: Colors.grey),
+      ),
+      maxLines: maxLines,
+      onChanged: onChanged,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '$label cannot be empty';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildNoProfilePrompt() {
+    return Column(
+      children: [
+        const Text(
+          'No profile found!',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          'It looks like you haven\'t set up your profile yet. Please add your details below to get started!',
+          style: TextStyle(fontSize: 16, color: Colors.black54),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 30),
+        _buildProfileForm(),
+      ],
+    );
+  }
+
+  Widget _buildProfileForm() {
+    return Form(
+      key: controller.formKey,
+      child: Column(
+        children: [
+          _buildTextFormField(
+            'Username',
+            '',
+            (value) {
+              controller.username.value = value;
+            },
+            icon: Icons.account_circle,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Location',
+            '',
+            (value) {
+              controller.location.value = value;
+            },
+            icon: Icons.location_on,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Specialization',
+            '',
+            (value) {
+              controller.specialization.value = value;
+            },
+            icon: Icons.star,
+          ),
+          const SizedBox(height: 20),
+          _buildTextFormField(
+            'Bio',
+            '',
+            (value) {
+              controller.bio.value = value;
+            },
+            icon: Icons.edit,
+            maxLines: 4,
+          ),
+        ],
       ),
     );
   }
